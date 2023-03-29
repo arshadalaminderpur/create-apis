6e42f0f41429e0d2aa9d319bd9a0acc1481685f1
arshad <arshad.alam@olaelectric.com>
2023-03-30 00:10:48 +0530

    feature2 changes

--- src/main/java/com/example/createapis/controller/TransactionController.java
+++ src/main/java/com/example/createapis/controller/TransactionController.java
@@ -33,7 +33,7 @@ public class TransactionController {
     public int code(){
         int []arr={-1,-8,0,5,-9};
         func(1,arr);
-        log.info("arshad feature1 branch");
+        log.info("arshad feature2 branch");
         return 0;
 
 
