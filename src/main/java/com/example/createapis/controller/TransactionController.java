fcf32d72ff6775a9e3627074aff3df9811db489b
arshad <arshad.alam@olaelectric.com>
2023-03-30 11:37:34 +0530

    change method name

--- src/main/java/com/example/createapis/controller/TransactionController.java
+++ src/main/java/com/example/createapis/controller/TransactionController.java
@@ -30,7 +30,7 @@ public class TransactionController {
 
 
    @GetMapping("/code")
-    public int codeKaro(){
+    public int codeKaroMat(){
 
         int []arr={-1,-8,0,5,-9};
         func(1,arr);
